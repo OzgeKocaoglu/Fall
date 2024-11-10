f9076c750f7a12171bbd5c9b62a65d3cd1dc0a21
Özge Kocaoğlu <ozge.kocaoglu@outlook.com>
2024-11-10 14:30:13 +0300

    Api moved to another repository

--- api/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register, login } = require('../controllers/auth');
-
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
